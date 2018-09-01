285c29e52eae03b8c038102f9b2c9d135498aa9a
Mateusz Nowakowski <mateusz.nowakowski@capgemini.com>
2018-09-01 18:43:35 +0200

    config logging

--- src/main/java/pl/com/ugeonk/HelloApp.java
+++ src/main/java/pl/com/ugeonk/HelloApp.java
@@ -1,27 +1,37 @@
 package pl.com.ugeonk;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.com.ugeonk.service.DebuggingService;
 import pl.com.ugeonk.service.PrototypeService;
+
+import java.util.Arrays;
 
 public class HelloApp {
 
-    private static final Logger logger = LogManager.getLogger(HelloApp.class);
-
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring-*.xml");
-        String[] beans = context.getBeanDefinitionNames();
-        logger.info("Beans names:");
-        for(String beanName: beans){
-            logger.debug(beanName);
-        }
-        logger.warn("Beans count: " + context.getBeanDefinitionCount());
+        //ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring-*.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring-*.xml");
+        printSpringContextBeans(context);
 
-        //logger.error("error");
-        //logger.fatal("fatal");
+        DebuggingService debuggingService =context.getBean(DebuggingService.class);
+        debuggingService.testAllDebugLevel();
+
         context.getBean(PrototypeService.class);
+
+        //call predestroy methods
+        context.registerShutdownHook();
     }
 
+    private static void printSpringContextBeans(AbstractApplicationContext context) {
+        System.out.println("Beans names:");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .filter(n -> !n.contains("springframework"))
+                .forEach(System.out::println);
+
+        long noSpringBeansCount = Arrays.stream(context.getBeanDefinitionNames())
+                .filter(n -> !n.contains("springframework")).count();
+
+        System.out.println("Beans count: " + noSpringBeansCount);
+    }
 }
